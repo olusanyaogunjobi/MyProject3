b5a32ee03e26a25f1f910cbbfb58e737a809218b
deltagare <iogunjobi@gmail.com>
2021-06-06 22:30:29 +0200

    Calculator

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -26,11 +26,15 @@ import java.util.Scanner;
 
             System.out.println("Num1 : " + num1  + " " + "Num2 : " + num2);
             int result1 = addition(num1,num2);       // num1 - num2
-             int result2 = subtraction(num1,num2);
-            System.out.println(result1);
+            int result2 = subtraction(num1,num2);
+            int result3 = multiplication(num1, num2);
+            int result4 = division(num1, num2);
+            System.out.println();
             System.out.println(" The Add result is" + " " +result1);
             System.out.println(" The Subtract result is" + " " +result2);
-    }
+            System.out.println(" The Multiply result is" + " " +result3);
+            System.out.println(" The division result is" + " " +result4);
+           }
 
     public static int addition(int num1, int num2) {
                   return num1 + num2;
@@ -41,7 +45,16 @@ import java.util.Scanner;
     public static int subtraction(int num1, int num2){
 
              return num2 - num1;
-    }
+
+            }
+
+
+     public static int multiplication (int num1, int num2){
+                return num1 * num2;
+           }
+
+           public static int division (int num1, int num2){
+                 return num2 / num1;
+           }
 
     }
-
